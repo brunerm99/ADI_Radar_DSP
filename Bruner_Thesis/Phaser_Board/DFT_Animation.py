833ac0ccbd23b31e48b3817f4828b98b6c0bbfb5
Marshall Bruner <brunerm99@gmail.com>
2022-02-16 13:48:00 -0700

    Created DFT loop animation

---
+++ Bruner_Thesis/Phaser_Board/DFT_Animation.py
@@ -0,0 +1,223 @@
+# %%
+# Copyright (C) 2019 Analog Devices, Inc.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#     - Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     - Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#     - Neither the name of Analog Devices, Inc. nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#     - The use of this software may or may not infringe the patent rights
+#       of one or more patent holders.  This license does not release you
+#       from the requirement that you obtain separate licenses from these
+#       patent holders to use this software.
+#     - Use of the software either in source or binary form, must be run
+#       on or directly connected to an Analog Devices Inc. component.
+#
+# THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED.
+#
+# IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, INTELLECTUAL PROPERTY
+# RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+Simple FMCW demo with PHASER and ADALM-PLUTO
+waterfall plot modified from:  https://amyboyle.ninja/Pyqtgraph-live-spectrogram
+"""
+
+# %%
+
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import signal
+import faulthandler
+faulthandler.enable()
+
+# Signal processing stuff
+from numpy.fft import fft, ifft, fftshift, fftfreq
+from numpy import absolute, pi
+from target_detection import cfar
+
+import adi
+
+# %%
+
+# Instantiate all the Devices
+rpi_ip = "ip:phaser.local"  # IP address of the Raspberry Pi
+sdr_ip = "ip:192.168.2.1" # "192.168.2.1, or pluto.local"  # IP address of the Transreceiver Block
+
+try:
+    x = my_sdr.uri
+    print("Pluto already connected")
+except NameError:
+    print("Pluto not connected...")
+    my_sdr = adi.ad9361(uri=sdr_ip)
+
+time.sleep(0.5)
+
+try:
+    x = my_phaser.uri
+    print("cn0566 already connected")
+except NameError:
+    print("cn0566 not open...")
+    my_phaser = adi.CN0566(uri=rpi_ip, rx_dev=my_sdr)
+
+# Initialize both ADAR1000s, set gains to max, and all phases to 0
+my_phaser.configure(device_mode="rx")
+for i in range(0, 8):
+    my_phaser.set_chan_gain(i, 127)
+    my_phaser.set_chan_phase(i, 0)
+
+
+sample_rate = 2e6
+center_freq = 2.1e9
+signal_freq = 100e3
+num_slices = 200
+fft_size = 1024*16
+# fft_size = 2**10
+
+# Create radio
+'''This script is for Pluto Rev C, dual channel setup'''
+my_sdr.sample_rate = int(sample_rate)
+
+# Configure Rx
+my_sdr.rx_lo = int(center_freq)   # set this to output_freq - (the freq of the HB100)
+my_sdr.rx_enabled_channels = [0, 1]   # enable Rx1 (voltage0) and Rx2 (voltage1)
+my_sdr.rx_buffer_size = int(fft_size)
+my_sdr.gain_control_mode_chan0 = 'manual'  # manual or slow_attack
+my_sdr.gain_control_mode_chan1 = 'manual'  # manual or slow_attack
+my_sdr.rx_hardwaregain_chan0 = int(30)   # must be between -3 and 70
+my_sdr.rx_hardwaregain_chan1 = int(30)   # must be between -3 and 70
+# Configure Tx
+my_sdr.tx_lo = int(center_freq)
+my_sdr.tx_enabled_channels = [0, 1]
+my_sdr.tx_cyclic_buffer = True      # must set cyclic buffer to true for the tdd burst mode.  Otherwise Tx will turn on and off randomly
+my_sdr.tx_hardwaregain_chan0 = -88   # must be between 0 and -88
+my_sdr.tx_hardwaregain_chan1 = -0   # must be between 0 and -88
+
+# Enable TDD logic in pluto (this is for synchronizing Rx Buffer to ADF4159 TX input)
+#gpio = adi.one_bit_adc_dac(sdr_ip)
+#gpio.gpio_phaser_enable = True
+
+# Configure the ADF4159 Rampling PLL
+output_freq = 12.1e9
+BW = 500e6
+num_steps = 1000
+ramp_time = 1e3 # us
+my_phaser.frequency = int(output_freq/4) # Output frequency divided by 4
+my_phaser.freq_dev_range = int(BW/4) # frequency deviation range in Hz.  This is the total freq deviation of the complete freq ramp
+my_phaser.freq_dev_step = int(BW/num_steps) # frequency deviation step in Hz.  This is fDEV, in Hz.  Can be positive or negative
+my_phaser.freq_dev_time = int(ramp_time) # total time (in us) of the complete frequency ramp
+my_phaser.delay_word = 4095     # 12 bit delay word.  4095*PFD = 40.95 us.  For sawtooth ramps, this is also the length of the Ramp_complete signal
+my_phaser.delay_clk = 'PFD'     # can be 'PFD' or 'PFD*CLK1'
+my_phaser.delay_start_en = 0         # delay start
+my_phaser.ramp_delay_en = 0          # delay between ramps.  
+my_phaser.trig_delay_en = 0          # triangle delay
+my_phaser.ramp_mode = "continuous_triangular"     # ramp_mode can be:  "disabled", "continuous_sawtooth", "continuous_triangular", "single_sawtooth_burst", "single_ramp_burst"
+my_phaser.sing_ful_tri = 0           # full triangle enable/disable -- this is used with the single_ramp_burst mode 
+my_phaser.tx_trig_en = 0             # start a ramp with TXdata
+# Enable ADF4159 TX input and generate a single triangular ramp with each trigger
+# my_phaser.ramp_mode = "single_ramp_burst"     # ramp_mode can be:  "disabled", "continuous_sawtooth", "continuous_triangular", "single_sawtooth_burst", "single_ramp_burst"
+# my_phaser.sing_ful_tri = 1           # full triangle enable/disable -- this is used with the single_ramp_burst mode 
+# my_phaser.tx_trig_en = 1             # start a ramp with TXdata
+my_phaser.enable = 0                 # 0 = PLL enable.  Write this last to update all the registers
+
+"""
+    Print config
+"""
+print("""
+CONFIG:
+Sample rate: {sample_rate}MHz
+Num samples: 2^{Nlog2}
+Bandwidth: {BW}MHz
+Ramp time: {ramp_time}ms
+Output frequency: {output_freq}MHz
+""".format(sample_rate=sample_rate / 1e6, Nlog2=int(np.log2(fft_size)), 
+    BW=BW / 1e6, ramp_time=ramp_time / 1e3, output_freq=output_freq / 1e6))
+
+# Create a sinewave waveform
+fs = int(my_sdr.sample_rate)
+print("sample_rate:", fs)
+N = int(my_sdr.rx_buffer_size)
+fc = int(signal_freq / (fs / N)) * (fs / N)
+ts = 1 / float(fs)
+t = np.arange(0, N * ts, ts)
+i = np.cos(2 * np.pi * t * fc) * 2 ** 14
+q = np.sin(2 * np.pi * t * fc) * 2 ** 14
+iq = 1 * (i + 1j * q)
+
+# Send data
+my_sdr._ctx.set_timeout(0)
+my_sdr.tx([iq*0, iq])  # only send data to the 2nd channel (that's all we need)
+
+if __name__ == '__main__':
+    REAL_TIME_PLOT = True
+    # %%
+    """
+        Plot DFT using matplotlib
+    """
+    plt.ion()
+    # Create figure
+    fig, ax = plt.subplots()
+    plt.title("Received Signal - Frequency Domain")
+    plt.xlabel("frequency [MHz]")
+    plt.ylabel("dB")
+    # plt.ylim([-120, -20])
+
+    # %%
+
+    # Collect raw data buffer, take DFT, and do basic processing
+    print("Collecting data...", end="")
+    x_n = my_sdr.rx()
+    print("Done")
+    x_n = x_n[0] + x_n[1]
+
+    X_k = absolute(fft(x_n))
+    X_k = fftshift(X_k)
+
+    N = len(X_k)
+    freq = fftshift(fftfreq(N, 1 / sample_rate))
+
+    # CFAR parameters
+    num_guard_cells = 10
+    num_ref_cells = 30
+    bias = 2
+    # Get CFAR values and mask non-targets
+    cfar_values, targets_only = cfar(X_k, num_guard_cells, 
+        num_ref_cells, bias, cfar_method='greatest')
+
+    line1, = ax.plot(freq, 10 * np.log10(targets_only), c='b')
+    line2, = ax.plot(freq, 10 * np.log10(cfar_values), c='r')
+
+    while (REAL_TIME_PLOT):
+        print("Collecting data...", end="")
+        x_n = my_sdr.rx()
+        print("Done")
+        x_n = x_n[0] + x_n[1]
+
+        X_k = absolute(fft(x_n))
+        X_k = fftshift(X_k)
+
+        # Get CFAR values and mask non-targets
+        cfar_values, targets_only = cfar(X_k, num_guard_cells, 
+            num_ref_cells, bias, cfar_method='greatest')
+
+        line1.set_ydata(10 * np.log10(targets_only))
+        line2.set_ydata(10 * np.log10(cfar_values))
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+
+
