ae7799bfb3c7fe8ad741b5b34ef0f1409601de61
Marshall Bruner <brunerm99@gmail.com>
2022-02-22 14:26:29 -0700

    Masks everything under ABS of threshold

--- Bruner_Thesis/netCDF_Functions.py
+++ Bruner_Thesis/netCDF_Functions.py
@@ -1,18 +1,11 @@
 # %%
 # Imports
-%reset
+# %reset
 import netCDF4 as nc
 import numpy as np
-import pandas as pd
-
-# %%
-# Passed in parameters
-buffer_size = 2**12 
-adc_len = 2**12
 
 # %%
 # Create nc file
-filename = 'test.nc'
 def create_dataset(output_filename, data, buffer_size, adc_len):
     ds = nc.Dataset(output_filename, 'w', format='NETCDF4')
 
@@ -26,21 +19,21 @@ def create_dataset(output_filename, data, buffer_size, adc_len):
     q = ds.createVariable('q', 'f4', ('time', 'bin',))
     q.units = 'Quadrature voltage relative to full scale (2^{:0.0f} bits)'.format(np.log2(adc_len))
 
-    for row in data.iloc:
-        curr_time = i.shape[0]
+    for curr_time, row in enumerate(data):
         bins[:] = np.arange(buffer_size)
-        iq_data = list(map(lambda x : complex(x), row))
-        i[curr_time, :] = list(map(lambda x : x.real, iq_data))
-        q[curr_time, :] = list(map(lambda x : x.imag, iq_data))
-    print(i.shape)
+        i[curr_time, :] = list(map(lambda x : complex(x).real, row))
+        q[curr_time, :] = list(map(lambda x : complex(x).imag, row))
 
     # Close dataset when done
     ds.close()
 # %%
 # Read netCDF file
 
-output_filename = 'radar_data.nc'
-data = pd.read_csv('data.csv')
-create_dataset(output_filename, data, 2**14, 2**12)
+if __name__ == '__main__':
+    import pandas as pd
 
-ds_read = nc.Dataset(output_filename, 'r')
+    output_filename = 'radar_data.nc'
+    data = pd.read_csv('data.csv')
+    create_dataset(output_filename, data, 2**14, 2**12)
+
+    ds_read = nc.Dataset(output_filename, 'r')
