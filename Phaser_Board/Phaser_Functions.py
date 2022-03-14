5cb9124e27562b6b429b788a2873748bdffd3c36
Marshall Bruner <brunerm99@gmail.com>
2022-03-14 16:51:42 -0600

    Fixed beam steering and forming

--- Phaser_Board/Phaser_Functions.py
+++ Phaser_Board/Phaser_Functions.py
@@ -19,42 +19,28 @@ from scipy import signal
     @param scan_angle: Desired scan angle
 """
 def update_phases(my_phaser, scan_angle, output_freq, num_devs=2, num_channels=4):
-    # print('Scan angle: %0.2f' % scan_angle)
     # Calculate phase offsets from scan angle
     phases_rad = np.arange(0, num_devs * num_channels, 1, dtype=float)
     phase_diff = (2 * np.pi * output_freq * my_phaser.element_spacing *
         np.sin(scan_angle * np.pi / 180)) / my_phaser.c
     phases_rad *= phase_diff
     phases_deg = np.degrees(phases_rad)
-    # print(np.degrees(phases_rad))
 
-    # TODO add phase calibration offset
-
-    # Update each channel with calculated phases
-    for dev_index, (dev_name, dev_obj) in enumerate(my_phaser.devices.items()):
-        for channel_index, channel in enumerate(dev_obj.channels):
-            # print('Old: %i-%i: %0.4f degrees' % (dev_index, channel_index, channel.rx_phase))
-            channel.rx_phase = phases_deg[(dev_index * num_channels) + channel_index]
-            # print('New: %i-%i: %0.4f degrees' % (dev_index, channel_index, channel.rx_phase), 
-                # end='\n\n')
-            dev_obj.latch_rx_settings()
+    for index in range(num_devs * num_channels):
+        my_phaser.elements.get(index + 1).rx_phase = phases_deg[index]
+    my_phaser.latch_rx_settings()
 
 """
-    TODO: check if max Rx gain is actually 70 rather than 127
     update_gains()
     Description: Updates each channel's gain given a taper values.
     @param my_phaser: Phaser object to be modified.
     @param taper: Gain taper values for all elements
 """
-def update_gains(my_phaser, taper, max_phaser_gain, num_channels=4):
-    print('Setting gain weights:')
-    for dev_index, (dev_name, dev_obj) in enumerate(my_phaser.devices.items()):
-        for channel_index, channel in enumerate(dev_obj.channels):
-            print('Old: %i-%i: %0.4f' % (dev_index, channel_index, channel.rx_gain))
-            channel.rx_gain = max_phaser_gain * taper[(dev_index * num_channels) + channel_index]
-            print('New: %i-%i: %0.4f' % (dev_index, channel_index, channel.rx_gain), 
-                end='\n\n')
-            dev_obj.latch_rx_settings()
+def update_gains(my_phaser, taper, max_phaser_gain, num_channels=4, num_devs=2):
+    for index in range(num_devs * num_channels):
+        my_phaser.elements.get(index + 1).rx_gain = taper[index]
+        my_phaser.elements.get(index + 1).rx_attenuator = not bool(taper[index]) 
+    my_phaser.latch_rx_settings()
 
 
 """
